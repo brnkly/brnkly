4b2f05a6b9cacc72058b9f9e2096aa168ef89e3b
John Bennett <john@jtbennett.com>
2012-12-19 20:33:32 -0500

    Create Brnkly nuget package from psake

--- default.ps1
+++ default.ps1
@@ -5,16 +5,16 @@ Properties {
 	$buildartifacts_dir		= "$build_dir\"
 	$sln_file 				= "$base_dir\Brnkly.sln"
 	$release_dir 			= "$base_dir\Release"
-	$build_number_default	= if ("$assembly_version".length -gt 0) { "$assembly_version" } else { "1.0.0.7" }
-	$build_number			= if ("$env:BUILD_NUMBER".length -gt 0) { "$env:BUILD_NUMBER" } else { "$build_number_default" } 
-	$build_vcs_number		= if ("$env:BUILD_VCS_NUMBER".length -gt 0) { "$env:BUILD_VCS_NUMBER" } else { "0" } 
+	#$build_number_default	= if ("$assembly_version".length -gt 0) { "$assembly_version" } else { "1.0.7" }
+	#$build_number			= if ("$env:BUILD_NUMBER".length -gt 0) { "$env:BUILD_NUMBER" } else { "$build_number_default" } 
+	#$build_vcs_number		= if ("$env:BUILD_VCS_NUMBER".length -gt 0) { "$env:BUILD_VCS_NUMBER" } else { "0" } 
 }
 
 FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
 
 include .\psake_ext.ps1
 
-task default -depends Compile
+task default -depends CreateNuGetPackages
 
 task Verify45 {
 	if (Test-Net45Full) { Write-Host ".NET Framework 4.5 Full is installed" } 
@@ -29,26 +29,39 @@ task Clean {
 task Init -depends Verify45, Clean {
 
 	if($env:buildlabel -eq $null) {
-		$env:buildlabel = "777"
+		$env:buildlabel = "7"
 	}
 
 	exec { git update-index --assume-unchanged "$base_dir\CommonAssemblyInfo.cs" }
-	$commit = Get-Git-Commit
+	$commit = Get-Git-Commit;
 	(Get-Content "$base_dir\CommonAssemblyInfo.cs") | 
-		Foreach-Object { $_ -replace ".777.", ".$($env:buildlabel)." } |
+		Foreach-Object { $_ -replace ".07", ".$($env:buildlabel)" } |
 		Foreach-Object { $_ -replace "{commit}", $commit } |
-		Set-Content "$base_dir\CommonAssemblyInfo.cs" -Encoding UTF8
+		Set-Content "$base_dir\CommonAssemblyInfo.cs" -Encoding UTF8;
 	
-	New-Item $release_dir -itemType directory -ErrorAction SilentlyContinue | Out-Null
-	New-Item $buildartifacts_dir -itemType directory -ErrorAction SilentlyContinue | Out-Null
+	New-Item $release_dir -Type directory -ErrorAction SilentlyContinue | Out-Null;
+	New-Item $buildartifacts_dir -Type directory -ErrorAction SilentlyContinue | Out-Null;
 }
 
 task Compile -depends Init {
-	exec { msbuild "$sln_file" /p:OutDir="$buildartifacts_dir\" /p:Configuration=Release }
+	exec { & msbuild "$sln_file" /p:OutDir="$buildartifacts_dir\" /p:Configuration=Release }
 }
 
-task Publish -depends Compile {
+task CreateNuGetPackages -depends Compile {
+	$nuget_dir = "$build_dir\NuGet";
+	if (Test-Path $nuget_dir) { Remove-Item -Force -Recurse $nuget_dir; }
+	New-Item $nuget_dir -Type Directory | Out-Null;
+
+	New-Item "$nuget_dir\Brnkly\lib\net45" -Type Directory | Out-Null;
+	Copy-Item "$buildartifacts_dir\Brnkly.???" "$nuget_dir\Brnkly\lib\net45";
+
+	Copy-Item "$base_dir\Brnkly\Brnkly.nuspec" "$nuget_dir\Brnkly";
+	(Get-Content "$nuget_dir\Brnkly\Brnkly.nuspec") |
+		Foreach-Object { $_ -replace ".07", ".$($env:buildlabel)" } |
+		Set-Content "$nuget_dir\Brnkly\Brnkly.nuspec" -Encoding UTF8;
+
+	exec { & "$base_dir\.nuget\nuget.exe" pack "$nuget_dir\Brnkly\Brnkly.nuspec" -OutputDirectory $nuget_dir }
 }
 
-task Release -depends Publish { 
+task Release { 
 }
